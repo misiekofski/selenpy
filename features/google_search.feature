696f4f74afe0fd93362bba718e457376e622af75
Michal Dobrzycki <dobrzycki@gmail.com>
2019-09-29 19:19:06 +0200

    Added instructions and fixed tests

--- features/google_search.feature
+++ features/google_search.feature
@@ -4,3 +4,14 @@ Feature: Check search functionality on google home page
     Given I open the site "https://www.google.com"
     When I search by feeling lucky for phrase "Selenium"
     Then I am redirected to other site than "https://www.google.com"
+    
+    
+  Scenario Outline: Check that google search shows some results
+    Given I open the site "https://www.google.com"
+    When I search with normal search for phrase <search_phrase>
+    Then Results count is more than <results_count>
+    Examples:
+    | search_phrase|results_count |
+    | Selenium               | 10 |
+    | NU@#%(*DGNS%#SFDIUBW#% | 3  |
+    | pinasdgoinasdgoinasdg  | -1 |
--- pages/google_page.py
+++ pages/google_page.py
@@ -1,13 +1,28 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class GooglePage(BasePage):
     SEARCH_INPUT = (By.NAME, 'q')
     FEELING_LUCKY = (By.NAME, 'btnI')
+    SEARCH_BUTTON = (By.NAME, 'btnK')
+    RESULTS_LIST = (By.XPATH, '//h3[@class="LC20lb"]')
 
     def feeling_lucky_search(self, phrase):
         search_input = self.driver.find_element(*self.SEARCH_INPUT)
         search_input.send_keys(phrase)
         feeling_lucky = self.driver.find_element(*self.FEELING_LUCKY)
         feeling_lucky.click()
+
+    def normal_search(self, phrase):
+        search_input = self.driver.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase)
+        search_input.send_keys(Keys.ENTER)
+
+    def return_results_count(self):
+        try:
+            results_list = self.driver.find_elements(*self.RESULTS_LIST)
+            return len(results_list)
+        except:
+            return 0
